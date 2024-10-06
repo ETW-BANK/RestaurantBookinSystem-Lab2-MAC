cb32744f77541d78417041ac4589750c292f14ba
dinozoff1981 <tensaeg@yahoo.com>
2024-10-06 20:39:51 +0200

    tables fron end in progress

---
+++ RestaurantBookingFrontApp/Areas/Admin/Controllers/TablesController.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using RestaurantViewModels;
+using System.Text;
+
+namespace RestaurantBookingApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class TablesController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public TablesController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7232/api/Table/");
+        }
+
+        [HttpGet]
+        public IActionResult Index() => View();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllTables()
+        {
+            var response = await _httpClient.GetAsync("GetAllTables");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var serviceResponse = JsonConvert.DeserializeObject<List<TablesVM>>(data);
+                return Json(data);
+            }
+
+            return Json(new { data = new List<TablesVM>(), error = "Unable to retrieve Tables from the server." });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.PageTitle = "Create Table";
+            ViewBag.ButtonLabel = "Create";
+
+            return View(new TablesVM());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TablesVM table)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(table), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PostAsync("CreateNewTable", content);
+            TempData["success"] = "Table Created successfully";
+            return response.IsSuccessStatusCode ? RedirectToAction(nameof(Index)) : View(table);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var response = await _httpClient.GetAsync($"GetTable/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var tableResponse = JsonConvert.DeserializeObject<TablesVM>(data);
+
+                ViewBag.PageTitle = "Edit Table";
+                ViewBag.ButtonLabel = "Update";
+
+                if (tableResponse!= null)
+                {
+                    return View("Create", tableResponse);
+                }
+            }
+
+            return View("Error");
+        }
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(TablesVM table)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(table), Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync("Update", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+
+                TempData["success"] = "Table Info Updated successfully";
+            }
+            return response.IsSuccessStatusCode ? RedirectToAction("Index") : View(table);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var response = await _httpClient.GetAsync($"GetTable/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var serviceResponse = JsonConvert.DeserializeObject<TablesVM>(data);
+                if (serviceResponse!=null)
+                {
+
+                    return View(serviceResponse);
+                }
+            }
+
+            return View("Error");
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var response = await _httpClient.DeleteAsync($"DeleteTable/{id}");
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["success"] = "Table Deleted successfully";
+                return RedirectToAction("Index");
+            }
+
+            return View("Error");
+        }
+
+    }
+}
---
+++ RestaurantBookingFrontApp/wwwroot/js/TableList.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": 'Tables/GetAllTables',
+            "type": "GET",
+            "datatype": "json",
+            "dataSrc": "",
+        },
+        "columns": [
+            { "data": 'tableNumber', "width": "20%" },
+            { "data": 'numberOfSeats', "width": "20%" },
+            { "data": 'isAvialable', "width": "20%" },
+            {
+                "data": 'id',
+                "render": function (data) {
+                    return `
+                        <div class="w-75 btn-group" role="group">
+                            <a href="Tables/Edit?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i> Edit
+                            </a>
+                            <a href="Tables/Delete?id=${data}" class="btn btn-danger mx-2">
+                                <i class="bi bi-trash-fill"></i> Delete
+                            </a>
+                        </div>
+                    `;
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}

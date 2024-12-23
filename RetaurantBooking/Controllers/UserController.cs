0d4746b6305dfe1f7c69bdda6353bab017aec23b
dinozoff1981 <tensaeg@yahoo.com>
2024-12-23 12:10:12 +0100

    get all users completed

--- RetaurantBooking/Controllers/UserController.cs
+++ RetaurantBooking/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿
 using Microsoft.AspNetCore.Mvc;
+using RestaurantServices.Services;
 using RestaurantServices.Services.IServices;
 
 namespace RetaurantBooking.Controllers
@@ -27,81 +28,92 @@ namespace RetaurantBooking.Controllers
                 return NotFound(new { message = "User not found." });
             }
 
-            // Serialize and return complete user details
+
             var userDetails = new
             {
                 user.Id,
                 user.Name,
                 user.Email,
-              
+
             };
 
             return Ok(userDetails);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var users = await _userService.GetAllUsers();
+            if (users == null || !users.Any())
+            {
+                return NotFound(new { message = "No users found." });
+            }
+
+            return Ok(new { data = users });
+        }
+
+
+
+
+        //[HttpPost]
+        //public async Task<IActionResult> LockUser([FromBody] string id)
+        //{
+        //    var usertolock = await _dbContext.ApplicationUsers.FirstOrDefaultAsync(x => x.Id == id);
+
+        //    if (usertolock == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    if(usertolock.LockoutEnd!=null && usertolock.LockoutEnd > DateTime.Now)
+        //    {
+        //        usertolock.LockoutEnd = DateTime.Now;   
+        //    }
+
+        //    else
+        //    {
+        //        usertolock.LockoutEnd=DateTime.Now.AddDays(5);
+        //    }
+
+        //    return Ok(usertolock);
+        //}
+
+
+
+        //[HttpGet]
+        //public async Task<IActionResult> RoleManagment(string userId)
+        //{
+        //    try
+        //    {
+        //        var userRoles = await _userService.RoleManagment(userId); 
+        //        if (userRoles == null)
+        //        {
+        //            return NotFound(new { message = "User not found" });
+        //        }
+
+        //        return Ok(new { data = userRoles });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return BadRequest(new { message = ex.Message });
+        //    }
+        //}
+
+        //[HttpPost]
+        //public async  Task<IActionResult> RoleManagment(RoleManagmentVM roleManagmentVM)
+        //{
+        //   var newrole=_userService.RoleManagment(roleManagmentVM.ApplicationUser.Id);
+
+        //    if (newrole == null)
+        //    {
+        //        return BadRequest();
+        //    }
+        //    else
+        //    {
+        //      _userService.UpdateUserRole(roleManagmentVM);
+
+        //        return Ok();
+        //    }
+        //}
     }
-
-
-
-
-
-
-    //[HttpPost]
-    //public async Task<IActionResult> LockUser([FromBody] string id)
-    //{
-    //    var usertolock = await _dbContext.ApplicationUsers.FirstOrDefaultAsync(x => x.Id == id);
-
-    //    if (usertolock == null)
-    //    {
-    //        return BadRequest();
-    //    }
-    //    if(usertolock.LockoutEnd!=null && usertolock.LockoutEnd > DateTime.Now)
-    //    {
-    //        usertolock.LockoutEnd = DateTime.Now;   
-    //    }
-
-    //    else
-    //    {
-    //        usertolock.LockoutEnd=DateTime.Now.AddDays(5);
-    //    }
-
-    //    return Ok(usertolock);
-    //}
-
-
-
-    //[HttpGet]
-    //public async Task<IActionResult> RoleManagment(string userId)
-    //{
-    //    try
-    //    {
-    //        var userRoles = await _userService.RoleManagment(userId); 
-    //        if (userRoles == null)
-    //        {
-    //            return NotFound(new { message = "User not found" });
-    //        }
-
-    //        return Ok(new { data = userRoles });
-    //    }
-    //    catch (Exception ex)
-    //    {
-    //        return BadRequest(new { message = ex.Message });
-    //    }
-    //}
-
-    //[HttpPost]
-    //public async  Task<IActionResult> RoleManagment(RoleManagmentVM roleManagmentVM)
-    //{
-    //   var newrole=_userService.RoleManagment(roleManagmentVM.ApplicationUser.Id);
-
-    //    if (newrole == null)
-    //    {
-    //        return BadRequest();
-    //    }
-    //    else
-    //    {
-    //      _userService.UpdateUserRole(roleManagmentVM);
-
-    //        return Ok();
-    //    }
-    //}
 }

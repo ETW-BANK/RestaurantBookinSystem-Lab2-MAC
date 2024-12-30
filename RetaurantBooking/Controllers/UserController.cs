0d9fa499a45f0a83005dc3cd57145c729c18ed37
dinozoff1981 <tensaeg@yahoo.com>
2024-12-30 13:29:01 +0100

    code clenup

--- RetaurantBooking/Controllers/UserController.cs
+++ RetaurantBooking/Controllers/UserController.cs
@@ -43,64 +43,6 @@ namespace RetaurantBooking.Controllers
             return Ok(new { data = users });
         }
 
-        //[HttpPost]
-        //public async Task<IActionResult> LockUser([FromBody] string id)
-        //{
-        //    var usertolock = await _dbContext.ApplicationUsers.FirstOrDefaultAsync(x => x.Id == id);
-
-        //    if (usertolock == null)
-        //    {
-        //        return BadRequest();
-        //    }
-        //    if(usertolock.LockoutEnd!=null && usertolock.LockoutEnd > DateTime.Now)
-        //    {
-        //        usertolock.LockoutEnd = DateTime.Now;   
-        //    }
-
-        //    else
-        //    {
-        //        usertolock.LockoutEnd=DateTime.Now.AddDays(5);
-        //    }
-
-        //    return Ok(usertolock);
-        //}
-
-
-
-        //[HttpGet]
-        //public async Task<IActionResult> RoleManagment(string userId)
-        //{
-        //    try
-        //    {
-        //        var userRoles = await _userService.RoleManagment(userId); 
-        //        if (userRoles == null)
-        //        {
-        //            return NotFound(new { message = "User not found" });
-        //        }
-
-        //        return Ok(new { data = userRoles });
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        return BadRequest(new { message = ex.Message });
-        //    }
-        //}
-
-        //[HttpPost]
-        //public async  Task<IActionResult> RoleManagment(RoleManagmentVM roleManagmentVM)
-        //{
-        //   var newrole=_userService.RoleManagment(roleManagmentVM.ApplicationUser.Id);
-
-        //    if (newrole == null)
-        //    {
-        //        return BadRequest();
-        //    }
-        //    else
-        //    {
-        //      _userService.UpdateUserRole(roleManagmentVM);
-
-        //        return Ok();
-        //    }
-        //}
+       
     }
 }

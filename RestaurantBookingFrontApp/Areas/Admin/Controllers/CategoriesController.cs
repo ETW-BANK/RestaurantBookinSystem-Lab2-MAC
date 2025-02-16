4a35d04f129d6428b8ea72b22d831092af4daaf0
dinozoff1981 <tensaeg@yahoo.com>
2025-02-16 04:25:47 +0100

    frontend in progress

---
+++ RestaurantBookingFrontApp/Areas/Admin/Controllers/CategoriesController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Restaurant.Utility;
+using RestaurantViewModels;
+using System.Text;
+
+namespace RestaurantBookingFrontApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = StaticData.Role_Admin)]
+    public class CategoriesController : Controller
+    {
+        private readonly HttpClient _httpClient;
+
+        public CategoriesController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:7232/api/Category/");
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllCategories()
+        {
+            var response = await _httpClient.GetAsync("GetAllCategories");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var serviceResponse = JsonConvert.DeserializeObject<List<CategoryVM>>(data);
+                return Json(new { data = serviceResponse });
+            }
+
+            return Json(new { data = new List<CategoryVM>(), error = "Unable to retrieve categories from the server." });
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View(new CategoryVM());
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryVM category)
+        {
+            var content = new StringContent(JsonConvert.SerializeObject(category), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync("CreateNewCategory", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                TempData["success"] = "Category created successfully.";
+                return RedirectToAction(nameof(Index));
+            }
+
+            
+            return View(category);
+        }
+
+    }
+}
---
+++ RestaurantBookingFrontApp/wwwroot/js/CategoryList.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": 'Categories/GetAllCategories',
+            "type": "GET",
+            "dataSrc": "data",
+        },
+        "columns": [
+            { "data": 'id', "width": "20%" },
+            { "data": 'name', "width": "20%" },
+            { "data": 'description', "width": "20%" },
+            {
+                "data": 'id',
+                "render": function (data, type, row) {
+                    return `
+        <div class="w-75 btn-group" role="group">
+            <a href="Tables/Edit?id=${row.id}" class="btn btn-primary mx-2">
+                <i class="bi bi-pencil-square"></i> Edit
+            </a>
+            <a href="Tables/Delete?id=${row.id}" class="btn btn-danger mx-2">
+                <i class="bi bi-trash-fill"></i> Delete
+            </a>
+        </div>
+    `;
+                },
+                "width": "15%"
+            }
+        ]
+    });
+}

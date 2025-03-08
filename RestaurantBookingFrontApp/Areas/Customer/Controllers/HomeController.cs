af253c57f648559e5af868f8390ead45c20bf50f
dinozoff1981 <tensaeg@yahoo.com>
2025-03-08 17:17:25 +0100

    added Category Selection in the home page.

--- RestaurantBookingFrontApp/Areas/Customer/Controllers/HomeController.cs
+++ RestaurantBookingFrontApp/Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using RestaurantBookingFrontApp.Models;
+using RestaurantViewModels;
 using System.Diagnostics;
+using System.Net.Http;
 
 
 namespace RestaurantBookingFrontApp.Areas.Customer.Controllers
@@ -12,19 +15,43 @@ namespace RestaurantBookingFrontApp.Areas.Customer.Controllers
     {
 
         private readonly ILogger<HomeController> _logger;
-      
-        public HomeController(ILogger<HomeController> logger)
+        private readonly HttpClient _httpClient;
+        public HomeController(ILogger<HomeController> logger, HttpClient httpClient)
         {
             _logger = logger;
-         
-     
+            _httpClient = httpClient;
+            _httpClient.BaseAddress = new Uri("https://localhost:44307/api/Category/");
+            _httpClient = httpClient;
         }
 
-        public IActionResult Index()
+
+
+        public async Task<IActionResult> Index()
         {
-            
-            return View();
+            var response = await _httpClient.GetAsync("GetCategories");
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                var categories = JsonConvert.DeserializeObject<List<CategoryVM>>(data);
+
+                // Ensure full URL for each category's ImageUrl
+                foreach (var category in categories)
+                {
+                    if (!string.IsNullOrEmpty(category.ImageUrl))
+                    {
+                        category.ImageUrl = $"https://localhost:44307{category.ImageUrl}";
+                    }
+                }
+
+                return View(categories);
+            }
+
+            TempData["error"] = "Unable to retrieve categories from the server.";
+            return View(new List<CategoryVM>());
         }
+
+
 
         public IActionResult Privacy()
         {
